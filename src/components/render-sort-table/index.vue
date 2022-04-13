5faff1f1512b65f0b893e2f489e0212a75a7f642
wy <mantou001@qq.com>
2022-04-13 20:27:48 +0800

    jxx

---
+++ src/components/render-sort-table/index.vue
@@ -0,0 +1,26 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: "vue-table",
+  props: {
+    columns: {
+      type: Array,
+      default: () => [],
+    },
+    data: {
+      type: Array,
+      default: () => [],
+    }
+  },
+  data() {
+
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/my-page/render-sort-table.vue
@@ -0,0 +1,16 @@
+<template>
+  <vue-sort-table></vue-sort-table>
+</template>
+
+<script>
+import VueSortTable from "../../components/render-sort-table/index.vue";
+
+export default {
+  name: "sort-table",
+  components: { VueSortTable },
+}
+</script>
+
+<style>
+
+</style>
